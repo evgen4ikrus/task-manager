56135f500ece929bf8e60e9d2742ed31a4c011d5
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-09-30 21:23:38 +0300

    add Employee, Task, Position, Project models

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Employee, Position, Project, Task
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    pass
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,63 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    title = models.CharField('Должность', max_length=50)
+
+    class Meta:
+        verbose_name = 'Должность сотрудника'
+        verbose_name_plural = 'Должности сотрудников'
+
+    def __str__(self):
+        return self.title
+
+
+class Employee(models.Model):
+    full_name = models.CharField('ФИО', max_length=100)
+    position = models.ForeignKey('Position', verbose_name='Должность', on_delete=models.CASCADE, related_name='employees')
+
+    class Meta:
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+    def __str__(self):
+        return self.full_name
+
+
+class Project(models.Model):
+    STATUSES = [
+        ('active', 'Активный'),
+        ('inactive', 'Неактивный')
+    ]
+    title = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание', blank=True)
+    manager = models.ForeignKey('Employee', verbose_name='Руководитель', on_delete=models.CASCADE, related_name='managers')
+    status = models.CharField('Статус', max_length=10, choices=STATUSES)
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+    def __str__(self):
+        return self.title
+
+
+class Task(models.Model):
+    STATUSES = [
+        ('new', 'Новый'),
+        ('active', 'В работе'),
+        ('completed', 'Завершена')
+    ]
+    title = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание', blank=True)
+    project = models.ForeignKey('Project', verbose_name='Проект', on_delete=models.CASCADE, related_name='tasks')
+    deadline = models.DateTimeField('Выполнить до')
+    status = models.CharField('Статус', max_length=10, choices=STATUSES, default='new')
+    performers = models.ManyToManyField('Employee', verbose_name='Исполнители', related_name='tasks', blank=True)
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
