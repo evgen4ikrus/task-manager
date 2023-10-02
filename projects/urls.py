22072cd6d0422f405b2764cfd60556dd1bbc2727
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-10-02 17:34:00 +0300

    improve display of data on pages

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,6 @@ from projects.views import ProjectDetailView, ProjectListView, TaskDetailView
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name="project-list"),
-    path('<int:pk>/', ProjectDetailView.as_view(), name="project-detail"),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name="project-detail"),
     path('task/<int:pk>/', TaskDetailView.as_view(), name="task-detail"),
 ]
