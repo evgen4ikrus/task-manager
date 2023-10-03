ff3c3d165ee64361db25de1d019db1ce6326dd67
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-10-03 20:30:05 +0300

    add filtering by status

--- projects/views.py
+++ projects/views.py
@@ -10,10 +10,23 @@ class ProjectListView(ListView):
     paginate_by = 50
     template_name = 'project_list.html'
     context_object_name = 'projects'
+    requested_project_status = 'all'
 
-    def get_queryset(self):
-        queryset = super(ProjectListView, self).get_queryset().select_related('manager')
-        return queryset
+    def get_context_data(self, **kwargs):
+        context = super(ProjectListView, self).get_context_data(**kwargs)
+        context['statuses'] = dict(Project.STATUSES)
+        context['requested_project_status'] = self.requested_project_status
+        return context
+
+    def get_queryset(self, **kwargs):
+        project_status = self.request.GET.get("project_status")
+        if project_status:
+            ProjectListView.requested_project_status = project_status
+        if ProjectListView.requested_project_status == 'all':
+            return super(ProjectListView, self).get_queryset().all().select_related('manager')
+        return super(ProjectListView, self).get_queryset().filter(
+            status=ProjectListView.requested_project_status).select_related('manager')
+
 
 
 class ProjectDetailView(DetailView):
